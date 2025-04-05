8beda0ac6c8cb53c54e86fcfb2eb6820d6af8155
RoRaut2001 <137515629+RoRaut2001@users.noreply.github.com>
2025-04-05 18:55:34 +0530

    change base-url

--- src/app/constants/api-urls.ts
+++ src/app/constants/api-urls.ts
@@ -1,8 +1,8 @@
 
 /**** Exporting object of URL tree ****/
 export const API_URL = {
-  BASE_URL: 'https://52.202.205.180:8080/',
-  // BASE_URL: 'https://solcon-backend.onrender.com/',
+  // BASE_URL: 'https://52.202.205.180:8080/',
+  BASE_URL: 'https://solcon-backend.onrender.com/',
   // BASE_URL: 'http://192.168.1.13:8080/',
 
   /**** Auth URL Object ****/
